6632acbbb2a612529a5861c2eb96d0cecf443150
gimi <g@1998.ws>
2020-01-10 16:35:48 +0800

    12

--- compose-demo/java/docker-entrypoint.sh
+++ compose-demo/java/docker-entrypoint.sh
@@ -5,6 +5,6 @@ pwd
 mvn clean package -Dmaven.test.skip=true docker:build
 echo 'build success'
 pwd
-copy target/docker-compose-test-0.0.1-SNAPSHOT.jar compose-demo/java/docker-compose-test-0.0.1-SNAPSHOT.jar
+copy /root/gg/git-reop/docker-compose-test/target/docker-compose-test-0.0.1-SNAPSHOT.jar /root/gg/git-reop/docker-compose-test/compose-demo/java/docker-compose-test-0.0.1-SNAPSHOT.jar
 pwd
 cd compose-demo/java
